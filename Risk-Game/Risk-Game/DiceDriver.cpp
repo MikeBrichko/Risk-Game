543c87ff09ab73cedcfa31274b87a449649eb809
Seungwoo Kim <seungwookim08@gmail.com>
2019-10-11 18:44:12 -0400

    Minor fix for printing message

--- Risk-Game/Risk-Game/DiceDriver.cpp
+++ Risk-Game/Risk-Game/DiceDriver.cpp
@@ -3,13 +3,7 @@
 #include <istream>
 
 
-void validatateDiceContainer(std::vector<int> diceRolled) {
-	for (auto diceFace : diceRolled) {
-		std::cout << diceFace << std::endl;
-	}
-}
-
-/**int main() {
+int main() {
 	int armiesOnCountry = 0;
 	std::string s1 = "";
 
@@ -17,18 +11,18 @@ void validatateDiceContainer(std::vector<int> diceRolled) {
 	Dice* dice = new Dice();
 
 	int numOfRolls = 0;
-	std::cout << "Enter the amount of time you want to roll the dice ";
+	std::cout << "Enter the amount of time you want to roll the dice: ";
 	std::cin >> numOfRolls;
 
 	std::vector<int> diceRolled = std::vector<int>();
 	while (numOfRolls != 0) {
 		armiesOnCountry = rand() % 3 + 1;
 		diceRolled = dice->rollDice(armiesOnCountry);
-		//dice->printDiceFacePercentageRolled();
+		for (auto diceFace : diceRolled) {
+			std::cout << "Rolled value: " <<diceFace << std::endl;
+		}
 		numOfRolls--;
 	}
 	dice->printDiceFacePercentageRolled();
-	validatateDiceContainer(diceRolled);
-}**/
-
+}
 
