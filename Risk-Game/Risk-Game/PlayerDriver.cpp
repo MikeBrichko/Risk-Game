0ad5a15c12f6f0061234c83bfcc0121cedcf11df
Mansour <maqsoudi99@gmail.com>
2019-10-27 18:49:34 -0400

    Updated PlayerDriver.cpp

--- Risk-Game/Risk-Game/PlayerDriver.cpp
+++ Risk-Game/Risk-Game/PlayerDriver.cpp
@@ -10,19 +10,49 @@ int main() {
 
 	//player->printCountriesOwned();
 
-	//std::cout << "Add Countries that player owns" << std::endl;
-	Country* Alaska = new Country(1, "Alaska", 1, 5);
-	Country* Quebec = new Country(2, "Quebec", 1, 5);
-	Country* Ontario = new Country(3, "Ontario", 1, 5);
-	Map* gameMap = new Map(2);
+	Map* gameMap = new Map(6);
 
-	player->addCountryOwned(Alaska);
-	player->addCountryOwned(Quebec);
-	player->addCountryOwned(Ontario);
+	//std::cout << "Add Countries that player owns" << std::endl;
+	Country* Canada = new Country(1, "Canada", 1, 5);
+	Country* Argentina = new Country(2, "Argentina", 2, 5);
+	Country* Spain = new Country(3, "Spain", 3, 5);
+	
+
+	player->addCountryOwned(Canada);
+	player->addCountryOwned(Argentina);
+	player->addCountryOwned(Spain);
 
 	player->printCountriesOwned();
 	std::cout << std::endl;
+	
+	//Create a second player for testing purposes----------------------------------------------------------------------
+	Player* player2 = new Player(1, "Player 2");
 
+	Country* USA = new Country(4, "USA", 1, 5);
+	Country* Mexico = new Country(5, "Mexico", 2, 5);
+	Country* Brazil = new Country(6, "Brazil", 2, 5);
+
+	player2->addCountryOwned(USA);
+	player2->addCountryOwned(Mexico);
+	player2->addCountryOwned(Brazil);
+	//------------------------------------------------------------------------------------------------------------------
+	
+	//Assign neighbours for each country
+	Canada->addNeighbour(USA);
+	Canada->addNeighbour(Spain);
+	Argentina->addNeighbour(Brazil);
+	Argentina->addNeighbour(Mexico);
+	Spain->addNeighbour(Canada);
+	USA->addNeighbour(Canada);
+	USA->addNeighbour(Mexico);
+	Mexico->addNeighbour(USA);
+	Mexico->addNeighbour(Brazil);
+	Mexico->addNeighbour(Argentina);
+	Brazil->addNeighbour(Mexico);
+	Brazil->addNeighbour(Argentina);
+
+
+	
 	//Validate Cards in Hand
 	player->printCarsInHand();
 
@@ -45,4 +75,7 @@ int main() {
 
 	//Validate Fortify
 	//player->fortifiy();
+
+
+
 }
