d255493bad0911b5850313d3d3c1d4a354f8636e
Mansour <maqsoudi99@gmail.com>
2019-11-09 13:26:17 -0500

    updated GameObservers.h

--- Risk-Game/Risk-Game/GameObservers.h
+++ Risk-Game/Risk-Game/GameObservers.h
@@ -1,2 +1,27 @@
 #pragma once
-//empty for now
+#include <list>
+
+//Abstract class Observer
+class Observer   
+{
+public:
+	~Observer();
+	virtual void Update() = 0;
+protected:
+	Observer();
+};
+
+
+//Class Subject (Observable)
+class Subject 
+{
+public:
+	virtual void Attach(Observer* o);
+	virtual void Detach(Observer* o);
+	virtual void Notify();
+	Subject();
+	~Subject();
+private:
+	std::list<Observer*> *observers;
+
+};
